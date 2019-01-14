30ec275cb5d386aeefc79b9e1376aa013e191d66
rubenbb <36450070+rubenbb@users.noreply.github.com>
2019-01-14 15:57:02 +0100

    Update foo.py

--- Part4/python/foo.py
+++ Part4/python/foo.py
@@ -11,14 +11,17 @@ i = 0
 
 def incrementingFunction():
     global i
-    # TODO: increment i 1_000_000 times
+    for j in range(0, 1000000):
+        i = i + 1
 
 def decrementingFunction():
     global i
-    # TODO: decrement i 1_000_000 times
+    for j in range(0, 1000000):
+        i = i - 1
 
 
-
+        
+        
 def main():
     # TODO: Something is missing here (needed to print i)
 
